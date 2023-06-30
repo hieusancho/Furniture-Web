a29092608a3ac79bb39f27d9cc4634db7880607e
hieusancho <85896001+hieusancho@users.noreply.github.com>
2023-06-30 15:47:23 +0700

    upload source code

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import { testimonial } from '../data';
+import TestimonialSlider from './TestimonialSlider';
+
 
 const Testimonial = () => {
-  return <div>Testimonial</div>;
+  const {image,title}=testimonial;
+
+  return <section className='section'>
+    <div className='container mx-auto'>
+      <div className='flex flex-col lg:flex-row lg:gap-x-10 gap-y-2 '>
+
+        <div className='lg:max-w-[50%]'>
+          {/* title */}
+          <h2 className='title mb-9'>{title}</h2>
+          {/* slider */}
+          <TestimonialSlider />
+        </div>
+
+        <div className='order-1'> 
+          <img src={image.type} alt=""/>
+        </div>
+
+      </div>
+    </div>
+  </section>
 };
 
 export default Testimonial;
